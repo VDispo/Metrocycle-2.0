f6884b3ac2dc080268e03b26a51550de6c453fd5
Jan Paul Batrina <jmbatrina@up.edu.ph>
2024-01-06 15:30:10 +0800

    Generate 3 road segments in front/back of vehicle
    Done so that fictional road generation is "hidden"
    from driver by ensuring that road creation/deletion
    is done outside of driver's vision.
    
    Value of 3 road segments is only an eyeball estimate

--- Assets/FictionalRoadGenerator.cs
+++ Assets/FictionalRoadGenerator.cs
@@ -9,6 +9,9 @@ public class FictionalRoadGenerator : MonoBehaviour
     public GameObject backColliderPrefab;
     public GameObject motorObject;
 
+    // number of roads before and after current vehicle position which are loaded
+    // e.g. a value of 3 means 3 road units on front and back of the vehicle
+    public int bufferRoadCount;
     public Vector3 frontColliderOffset;
     public Vector3 backColliderOffset;
 
@@ -21,6 +24,12 @@ public class FictionalRoadGenerator : MonoBehaviour
     void Start()
     {
         floor1 = Instantiate(floorPrefab, new Vector3(0,0,0), Quaternion.identity);
+
+        float floorZLength = floor1.GetComponent<Renderer>().bounds.size.z;
+        for (int i = 0; i < bufferRoadCount; ++i) {
+            Instantiate(floorPrefab, new Vector3(0,0,floorZLength*(i+1)), Quaternion.identity);
+            Instantiate(floorPrefab, new Vector3(0,0,-floorZLength*(i+1)), Quaternion.identity);
+        }
 
         offset = floor1.transform.position + frontColliderOffset;
         frontCollider = Instantiate(frontColliderPrefab, offset, frontColliderPrefab.transform.rotation);
