4b036ed6bce6f397d7991ae4247339d41f70d6d1
Jan Paul Batrina <jmbatrina@up.edu.ph>
2024-01-11 06:33:52 +0800

    Pause game after showing gameover popup

--- Assets/ObstacleCollide.cs
+++ Assets/ObstacleCollide.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ObstacleCollide : MonoBehaviour
 {
+    public GameObject gameoverPopup;
     void OnTriggerEnter (Collider other)
     {
         Debug.Log("Obstacle hit by" + other.gameObject.name);
+        gameoverPopup.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void restartGame() {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
