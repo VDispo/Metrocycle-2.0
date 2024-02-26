8b4ba9f59bc08e002fceb6a37050423ad6ac46bd
Jan Paul Batrina <jmbatrina@up.edu.ph>
2024-02-26 17:29:57 +0800

    Intersection: handle case when user enters antiparallel adjacent lane

--- Assets/Scripts/IntersectionChecker.cs
+++ Assets/Scripts/IntersectionChecker.cs
@@ -75,63 +75,83 @@ public class IntersectionChecker : MonoBehaviour
     public void laneDetectEntered(GameObject laneDetect) {
         Debug.Log("Collision with" + laneDetect);
         int idx = GetLaneDetectIndex(laneDetect);
+        bool isEntry = false;
         if (entryIdx == -1) {
             entryIdx = idx;
             Debug.Log("Entry:" + entryIdx);
-            return;
+            isEntry = true;
         }
-
-        // "rotate" perspective to either Idx 0 or 1
-        // e.g. if driver came from Idx 9 and drove to Idx 15,
-        // that is equivalent to driving from Idx 1 to Idx 7
-        // where 1 = 9-9 + (9%2), 7 = 15-9 + (9%2)
-        Debug.Log("Exit:" + idx);
-        idx = idx - entryIdx + (entryIdx % 2);
-        if (idx < 0) {
-            // e.g. from Idx 8 to Idx 0; 0 - 8 = -8
-            // but this is just a 180 inversion of Idx 0 to 8
-            // hence we expect exit idx to be 8 = -8 + 16
-            idx += laneDetects.Length;
+        else {
+            // "rotate" perspective to either Idx 0 or 1
+            // e.g. if driver came from Idx 9 and drove to Idx 15,
+            // that is equivalent to driving from Idx 1 to Idx 7
+            // where 1 = 9-9 + (9%2), 7 = 15-9 + (9%2)
+            Debug.Log("Exit:" + idx);
+            idx = idx - entryIdx + (entryIdx % 2);
+            if (idx < 0) {
+                // e.g. from Idx 8 to Idx 0; 0 - 8 = -8
+                // but this is just a 180 inversion of Idx 0 to 8
+                // hence we expect exit idx to be 8 = -8 + 16
+                idx += laneDetects.Length;
+            }
         }
 
         PopupType type = PopupType.INFO;
         string popupText = "";
 
-        // TODO: use PopupType.WARNING for bad
-        if (invalid_WrongWayIdx.Contains(idx)) {
-            Debug.Log("Invalid Wrong Way " + idx);
-            type = PopupType.ERROR;
-            popupText = wrongWayText;
-        }
-        else if (entryIdx == 0){
-            Debug.Log("From Left to " + idx);
-            if (bad_LeftLaneRightTurnIdx.Contains(idx)) {
+        if (isEntry) {
+            if ((entryIdx % 4) >= 2) {
+                // Refer to legend, every 3rd or 4th Idx is in reverse direction
+                Debug.Log("Entered Wrong Way " + entryIdx);
                 type = PopupType.ERROR;
-                popupText = leftLaneRightTurnText;
-            }
-            else if (bad_LeftLaneLeftTurnIdx.Contains(idx)) {
-                type = PopupType.ERROR;
-                popupText = leftLaneLeftTurnText;
-            }
-            else if (bad_LeftLaneUTurnIdx.Contains(idx)) {
-                type = PopupType.ERROR;
-                popupText = leftLaneUTurnText;
+                popupText = wrongWayText + " Entry";
+
+                // "Forgive" driver, set entryIdx to adjacent valid entry lane
+                // e.g. If they entered via Idx 2, correct to Idx 4
+                // e.g. wrong entry at Idx 14; correctedt to 14+2 = Idx 16 = Idx 0
+                entryIdx = (entryIdx+2) % laneDetects.Length;
             }
         }
         else {
-            Debug.Log("From Right to " + idx);
-            if (bad_RightLaneRightTurnIdx.Contains(idx)) {
+            // TODO: use PopupType.WARNING for bad
+            if (invalid_WrongWayIdx.Contains(idx)) {
+                Debug.Log("Invalid Wrong Way " + idx);
                 type = PopupType.ERROR;
-                popupText = rightLaneRightTurnText;
+                popupText = wrongWayText;
             }
-            else if (bad_RightLaneLeftTurnIdx.Contains(idx)) {
-                type = PopupType.ERROR;
-                popupText = rightLaneLeftTurnText;
+            else if (entryIdx == 0){
+                Debug.Log("From Left to " + idx);
+                if (bad_LeftLaneRightTurnIdx.Contains(idx)) {
+                    type = PopupType.ERROR;
+                    popupText = leftLaneRightTurnText;
+                }
+                else if (bad_LeftLaneLeftTurnIdx.Contains(idx)) {
+                    type = PopupType.ERROR;
+                    popupText = leftLaneLeftTurnText;
+                }
+                else if (bad_LeftLaneUTurnIdx.Contains(idx)) {
+                    type = PopupType.ERROR;
+                    popupText = leftLaneUTurnText;
+                }
             }
-            else if (bad_RightLaneUTurnIdx.Contains(idx)) {
-                type = PopupType.ERROR;
-                popupText = rightLaneUTurnText;
+            else {
+                Debug.Log("From Right to " + idx);
+                if (bad_RightLaneRightTurnIdx.Contains(idx)) {
+                    type = PopupType.ERROR;
+                    popupText = rightLaneRightTurnText;
+                }
+                else if (bad_RightLaneLeftTurnIdx.Contains(idx)) {
+                    type = PopupType.ERROR;
+                    popupText = rightLaneLeftTurnText;
+                }
+                else if (bad_RightLaneUTurnIdx.Contains(idx)) {
+                    type = PopupType.ERROR;
+                    popupText = rightLaneUTurnText;
+                }
             }
+
+            // reset entryIdx
+            entryIdx = -1;
         }
 
         Debug.Log(popupText);
@@ -146,8 +166,5 @@ public class IntersectionChecker : MonoBehaviour
         default:
             break;
         }
-
-        // reset entryIdx
-        entryIdx = -1;
     }
 }
