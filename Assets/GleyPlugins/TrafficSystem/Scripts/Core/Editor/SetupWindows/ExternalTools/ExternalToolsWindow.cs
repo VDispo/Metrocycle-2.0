079d1cfb2de14f6999a7c1105a2c27a75926c993
Jan Paul Batrina <jmbatrina@up.edu.ph>
2024-03-14 01:35:02 +0800

    MTS-ER3D: Fix External Tools, show ER3D immediately
    supercede commit ed64f69

--- Assets/GleyPlugins/TrafficSystem/Scripts/Core/Editor/SetupWindows/ExternalTools/ExternalToolsWindow.cs
+++ Assets/GleyPlugins/TrafficSystem/Scripts/Core/Editor/SetupWindows/ExternalTools/ExternalToolsWindow.cs
@@ -8,18 +8,18 @@ namespace GleyTrafficSystem
         protected override void TopPart()
         {
             base.TopPart();
-            // EditorGUILayout.Space();
-            // if (GUILayout.Button("Easy Roads"))
-            // {
+            EditorGUILayout.Space();
+            if (GUILayout.Button("Easy Roads"))
+            {
                 window.SetActiveWindow(typeof(EasyRoadsSetup), true);
-            // }
-            // EditorGUILayout.Space();
-            //
-            // if (GUILayout.Button("Cidy 2"))
-            // {
-            //     window.SetActiveWindow(typeof(CidySetup), true);
-            // }
-            // EditorGUILayout.Space();
+            }
+            EditorGUILayout.Space();
+
+            if (GUILayout.Button("Cidy 2"))
+            {
+                window.SetActiveWindow(typeof(CidySetup), true);
+            }
+            EditorGUILayout.Space();
         }
     }
 }
