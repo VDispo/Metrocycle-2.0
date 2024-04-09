ba836bec5c5103bffc701de8d41e1d555f04f4c4
Jan Paul Batrina <jmbatrina@up.edu.ph>
2024-04-09 21:17:05 +0800

    popUp: Add functionality to buttons

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 /* To use, attach this script to the "Bike" prefab instance
  */
--- Assets/Scripts/popUp.cs
+++ Assets/Scripts/popUp.cs
@@ -1,15 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using TMPro;
 
 public class popUp : MonoBehaviour
 {
     public GameObject popUpSystem;
 
+    private Transform lastActiveSet;
+    private Transform popUpBox;
+    void Start()
+    {
+        lastActiveSet = null;
+        popUpBox = popUpSystem.transform.Find("popUpBox");
+
+        for (int i = 0; i < transform.childCount; ++i) {
+            Transform popupSet = transform.GetChild(i);
+            // Assume all buttons are direct children of a set
+            for (int j = 0; j < popupSet.childCount; ++j) {
+                Transform button = popupSet.GetChild(j);
+                Button buttonScript = button.GetComponent<Button>();
+                if (buttonScript == null) {
+                    continue;
+                }
+
+                // HACK: Just use name to determine function
+                if (button.name.StartsWith("start")
+                    || button.name.StartsWith("continue")
+                ) {
+                    buttonScript.onClick.AddListener(() => {
+                        closePopup(); GameManager.Instance.resumeGame();
+                    });
+                } else if (button.name.StartsWith("reset")) {
+                    buttonScript.onClick.AddListener(() => {
+                        closePopup(); GameManager.Instance.restartGame();
+                    });
+                } else if (button.name.StartsWith("menu")) {
+                    buttonScript.onClick.AddListener(() => {
+                        closePopup(); SceneManager.LoadScene("Main Menu");
+                    });
+                }
+            }
+        }
+    }
+
     public void popStart(string headerMessage, string bodyMessage)
     {
-        Transform popUpBox = popUpSystem.transform.Find("popUpBox");
         Transform startSet = popUpSystem.transform.Find("startSet");
 
         GameObject headerTextObject = startSet.Find("headerText").gameObject;
@@ -20,25 +58,17 @@ public class popUp : MonoBehaviour
         headerText.text = headerMessage;
         bodyText.text = bodyMessage;
 
-        startSet.gameObject.SetActive(true);
-        popUpBox.gameObject.SetActive(true);
-
-        GameManager.Instance.pauseGame();
+        showPopup(startSet);
     }
 
     public void popPause()
     {
-        Transform popUpBox = popUpSystem.transform.Find("popUpBox");
         Transform pauseSet = popUpSystem.transform.Find("pauseSet");
-        pauseSet.gameObject.SetActive(true);
-        popUpBox.gameObject.SetActive(true);
-
-        GameManager.Instance.pauseGame();
+        showPopup(pauseSet);
     }
 
     public void popPrompt(string headerMessage, string bodyMessage)
     {
-        Transform popUpBox = popUpSystem.transform.Find("popUpBox");
         Transform promptSet = popUpSystem.transform.Find("promptSet");
 
         GameObject headerTextObject = promptSet.Find("headerText").gameObject;
@@ -49,19 +79,36 @@ public class popUp : MonoBehaviour
         headerText.text = headerMessage;
         bodyText.text = bodyMessage;
 
-        promptSet.gameObject.SetActive(true);
-        popUpBox.gameObject.SetActive(true);
-
-        GameManager.Instance.pauseGame();
+        showPopup(promptSet);
     }
 
     public void popFinish()
     {
-        Transform popUpBox = popUpSystem.transform.Find("popUpBox");
         Transform finishSet = popUpSystem.transform.Find("finishSet");
-        finishSet.gameObject.SetActive(true);
+        showPopup(finishSet);
+    }
+
+    public void showPopup(Transform set, bool pauseGame = true)
+    {
+        set.gameObject.SetActive(true);
+        lastActiveSet = set;
         popUpBox.gameObject.SetActive(true);
 
-        GameManager.Instance.pauseGame();
+        if (pauseGame) {
+            GameManager.Instance.pauseGame();
+        }
+        Debug.Log("show Popup");
+    }
+    public void closePopup()
+    {
+        Debug.Log("Close Popup");
+        if (lastActiveSet == null) {
+            return;
+        }
+
+        lastActiveSet.gameObject.SetActive(false);
+        popUpBox.gameObject.SetActive(false);
+
+        GameManager.Instance.resumeGame();
     }
 }
