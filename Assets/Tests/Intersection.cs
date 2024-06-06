608b2f1970fd0994fa48048bcba45badf3fec0de
Jan Paul Batrina <jmbatrina@up.edu.ph>
2024-06-07 02:05:00 +0800

    Add more blinker and head check test cases
    -short blinker time
    -wrong blinker direction
    -expired head check
    -headcheck before blinker

--- Assets/Tests/Intersection.cs
+++ Assets/Tests/Intersection.cs
@@ -14,8 +14,11 @@ public class Intersection
     private blinkers blinkerScript;
     private bool isSceneLoaded = false;
 
-    private static float minBlinkerTime = 2f;
+    private static float minBlinkerTime = 1f;
+    // NOTE: For now, the maxHeadCheckDelay is hardcoded (see value in scene) since it needs to be determined before the UnitySetup above
+    // TODO:  maybe use a dynamic array for the ValueSource instead so that we can modify it in runtime?
     private static float leewayTime = 0.1f;
+    private static float maxHeadCheckDelay = 1f - leewayTime;
 
     [UnitySetUp]
     public IEnumerator TestIntersectionSetup()
@@ -53,7 +56,7 @@ public class Intersection
         });
 
         // Some time-based timers don't work if current clock (Time.time) is near 0, so wait for a few seconds
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(minBlinkerTime + maxHeadCheckDelay + leewayTime);
 }
 
     [UnityTest]
@@ -63,12 +66,20 @@ public class Intersection
     }
 
     [UnityTest]
-    public IEnumerator BlinkerAndHeadChecks([ValueSource(nameof(BlinkerAndHeadCheckTestCases))] IntersectionTestCase tc)
+    public IEnumerator BlinkerAndHeadCheckBasic([ValueSource(nameof(BlinkerAndHeadCheckBasicTestCases))] IntersectionTestCase tc)
     {
         yield return GenericIntersectionTest(tc, true);
     }
 
-    private IEnumerator GenericIntersectionTest(IntersectionTestCase tc, bool needHeadCheckAndBlinkers=false)
+    [UnityTest]
+    public IEnumerator BlinkerAndHeadCheck([ValueSource(nameof(BlinkerAndHeadCheckTestCases))] IntersectionTestCase tc)
+    {
+        yield return GenericIntersectionTest(tc, true, true);
+    }
+
+    private IEnumerator GenericIntersectionTest(IntersectionTestCase tc,
+                                                bool needHeadCheckAndBlinkers=false,
+                                                bool doAHeadCheckBeforeBlinker=false)
     {
         // NOTE: Intersection can be "rotated" by adding 2 to index (see IntersectionChecker.cs), so test all combinations
         for (int i = 0; i < intersectionScript.laneDetects.Length; i += 2) {
@@ -92,6 +103,12 @@ public class Intersection
             // Reset game to turn off blinkers, reset headcheck timers
             GameManager.Instance.resetSignal.Invoke();
             // --- Simulate the driver driving through the intersection by entering the lane at index *from* and exiting the lane at index *to*
+
+            if (doAHeadCheckBeforeBlinker) {
+                simulateHeadCheck(tc.dir);
+                yield return new WaitForSeconds(leewayTime);
+            }
+
             // Use blinker and do headcheck first before entering intersection
             if (needHeadCheckAndBlinkers) {
                 if (tc.doBlinker) {
@@ -99,14 +116,7 @@ public class Intersection
                     yield return new WaitForSeconds(tc.blinkerTime);
                 }
                 if (tc.doHeadCheck) {
-                    // NOTE: Currently, headcheck inputs are tied to KeyDown/KeyUp events which are hard to decouple
-                    //       and Unity does not have a native way to simulate Key Presses
-                    // HACK: to simulate headcheck, simply change last headcheck time
-                    if (tc.dir == Direction.LEFT) {
-                        GameManager.Instance.HeadCheckScript.leftCheckTime = Time.time;
-                    } else {
-                        GameManager.Instance.HeadCheckScript.rightCheckTime = Time.time;
-                    }
+                    simulateHeadCheck(tc.dir);
                     yield return new WaitForSeconds(tc.headCheckTime);
                 }
             }
@@ -165,11 +175,9 @@ public class Intersection
         yield return new IntersectionTestCase {from = 1, to = 15, expectedError = ErrorReason.INTERSECTION_LEFT_UTURN_FROM_OUTERLANE};
     }
 
-    private static IEnumerable BlinkerAndHeadCheckTestCases()
+    private static IEnumerable BlinkerAndHeadCheckBasicTestCases()
     {
         Debug.Log("MIN BLINKER TIME" + minBlinkerTime);
-        float maxHeadCheckDelay = GameManager.Instance?.HeadCheckScript.maxHeadCheckDelay ?? 0f;
-        maxHeadCheckDelay -= leewayTime;
 
         // First, test proper turning
         yield return new IntersectionTestCase {from = 0, to = 10, expectedError = ErrorReason.NOERROR, dir = Direction.LEFT,
@@ -229,6 +237,67 @@ public class Intersection
         };  // right turn
     }
 
+    private static IEnumerable BlinkerAndHeadCheckTestCases()
+    {
+        Debug.Log("MIN BLINKER TIME" + minBlinkerTime);
+
+        // Test short blinker time
+        yield return new IntersectionTestCase {from = 0, to = 10, expectedError = ErrorReason.SHORT_BLINKER_TIME, dir = Direction.LEFT,
+            doBlinker = true, blinkerTime = minBlinkerTime / 4,
+            doHeadCheck = true, headCheckTime = 0f,
+        };  // left turn
+        yield return new IntersectionTestCase {from = 0, to = 14, expectedError = ErrorReason.SHORT_BLINKER_TIME, dir = Direction.LEFT,
+            doBlinker = true, blinkerTime = minBlinkerTime / 4,
+            doHeadCheck = true, headCheckTime = 0f,
+        };   // left U-turn
+        yield return new IntersectionTestCase {from = 1, to = 3, expectedError = ErrorReason.SHORT_BLINKER_TIME, dir = Direction.RIGHT,
+            doBlinker = true, blinkerTime = minBlinkerTime / 4,
+            doHeadCheck = true, headCheckTime = 0f,
+        };  // right turn
+
+        // Test wrong blinker direction
+        yield return new IntersectionTestCase {from = 0, to = 10, expectedError = ErrorReason.WRONG_BLINKER, dir = Direction.RIGHT,
+            doBlinker = true, blinkerTime = minBlinkerTime,
+            doHeadCheck = true, headCheckTime = maxHeadCheckDelay,
+        };  // left turn
+        yield return new IntersectionTestCase {from = 0, to = 14, expectedError = ErrorReason.WRONG_BLINKER, dir = Direction.RIGHT,
+            doBlinker = true, blinkerTime = minBlinkerTime,
+            doHeadCheck = true, headCheckTime = maxHeadCheckDelay,
+        };   // left U-turn
+        yield return new IntersectionTestCase {from = 1, to = 3, expectedError = ErrorReason.WRONG_BLINKER, dir = Direction.LEFT,
+            doBlinker = true, blinkerTime = minBlinkerTime,
+            doHeadCheck = true, headCheckTime = maxHeadCheckDelay,
+        };  // right turn
+
+        // Test exoired head check (head check done, but too early, e.g. 10s before turn. Needs another head check)
+        yield return new IntersectionTestCase {from = 0, to = 10, expectedError = ErrorReason.EXPIRED_HEADCHECK, dir = Direction.LEFT,
+            doBlinker = true, blinkerTime = minBlinkerTime,
+            doHeadCheck = true, headCheckTime = maxHeadCheckDelay*2,
+        };  // left turn
+        yield return new IntersectionTestCase {from = 0, to = 14, expectedError = ErrorReason.EXPIRED_HEADCHECK, dir = Direction.LEFT,
+            doBlinker = true, blinkerTime = minBlinkerTime,
+            doHeadCheck = true, headCheckTime = maxHeadCheckDelay*2,
+        };   // left U-turn
+        yield return new IntersectionTestCase {from = 1, to = 3, expectedError = ErrorReason.EXPIRED_HEADCHECK, dir = Direction.RIGHT,
+            doBlinker = true, blinkerTime = minBlinkerTime,
+            doHeadCheck = true, headCheckTime = maxHeadCheckDelay*2,
+        };  // right turn
+
+        // Test head check done BEFORE blinker (head check is done by main test code)
+        yield return new IntersectionTestCase {from = 0, to = 10, expectedError = ErrorReason.NO_HEADCHECK_AFTER_BLINKER, dir = Direction.LEFT,
+            doBlinker = true, blinkerTime = minBlinkerTime + maxHeadCheckDelay*3,
+            doHeadCheck = false, headCheckTime = maxHeadCheckDelay,
+        };  // left turn
+        yield return new IntersectionTestCase {from = 0, to = 14, expectedError = ErrorReason.NO_HEADCHECK_AFTER_BLINKER, dir = Direction.LEFT,
+            doBlinker = true, blinkerTime = minBlinkerTime + maxHeadCheckDelay*3,
+            doHeadCheck = false, headCheckTime = maxHeadCheckDelay,
+        };   // left U-turn
+        yield return new IntersectionTestCase {from = 1, to = 3, expectedError = ErrorReason.NO_HEADCHECK_AFTER_BLINKER, dir = Direction.RIGHT,
+            doBlinker = true, blinkerTime = minBlinkerTime + maxHeadCheckDelay*3,
+            doHeadCheck = false, headCheckTime = maxHeadCheckDelay,
+        };  // right turn
+    }
+
     public struct IntersectionTestCase
     {
         public int from;
@@ -239,6 +308,18 @@ public class Intersection
         public float blinkerTime;       // Time between blinker and headcheck
         public bool doHeadCheck;
         public float headCheckTime;     // Time between head check and turn
+    }
+
+    private void simulateHeadCheck(Direction dir)
+    {
+        // NOTE: Currently, headcheck inputs are tied to KeyDown/KeyUp events which are hard to decouple
+        //       and Unity does not have a native way to simulate Key Presses
+        // HACK: to simulate headcheck, simply change last headcheck time
+        if (dir == Direction.LEFT) {
+            GameManager.Instance.HeadCheckScript.leftCheckTime = Time.time;
+        } else {
+            GameManager.Instance.HeadCheckScript.rightCheckTime = Time.time;
+        }
     }
 
     [OneTimeTearDown]
