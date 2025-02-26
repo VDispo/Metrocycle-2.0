2004444c0b0d8fbb5a2f71a251161932a835e290
shankencedric <legara.cedric@gmail.com>
2025-02-26 11:45:16 +0800

    fix: scenario start objective does not show

--- Assets/Scripts/ScenarioStart.cs
+++ Assets/Scripts/ScenarioStart.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Metrocycle;
+using TMPro;
 
 public class ScenarioStart : MonoBehaviour
 {
     public BikeType bikeType = BikeType.Motorcycle;
     public bool isTestScene = false;
+
+    public TMP_Text objectiveTextUI;
     public string ScenarioTitle { get; set; }
     public string ScenarioText { get; set; }
 
     // Update is called once per frame
     void Start()
     {
+        objectiveTextUI.text = ScenarioTitle;
         GameManager.Instance.PopupSystem.popStart(ScenarioTitle, ScenarioText);
         GameManager.Instance.setBikeType(bikeType);
         GameManager.Instance.isTestMode = isTestScene;
