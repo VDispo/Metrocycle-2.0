78e4f409f3d34a48360a97a81d756436a9486cc6
shankencedric <legara.cedric@gmail.com>
2025-03-23 18:44:45 +0800

    fix: translations for development

--- Assets/Scripts/LocalizationCache.cs
+++ Assets/Scripts/LocalizationCache.cs
@@ -5,19 +5,37 @@ using UnityEngine.Localization.Settings;
 
 public class LocalizationCache : MonoBehaviour
 {
+    /// <summary>
+    /// This script is meant to stay alive for the whole duration of the game to maximize caches.
+    /// Hence, it is in the Start Scene and is in DontDestroyOnLoad.
+    /// 
+    /// However, during development, we don't necessarily play from the Start Scene, this script
+    /// can be put in individual scenes, and this instance would be the one to live for the whole 
+    /// duration for the game.
+    /// </summary>
+
     public static LocalizationCache Instance;
     private Dictionary<string, string> cache = new();
 
     private void Awake()
     {
-        if (Instance != null) return;
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
         
         Instance = this;
         DontDestroyOnLoad(this);
     }
 
-    // Very useful for scripts that are referenced multiple times but have the same constant string to reference
-    // (e.g., for SpeedChecker.cs since it is in more than 5k elements just in the Commonwealth scene)
+    /// <summary>
+    /// Very useful for scripts that are referenced multiple times but have the same constant string to reference
+    /// (especially for SpeedChecker.cs since it is in more than 5000 elements just in the Commonwealth scene)
+    /// </summary>
+    /// <param name="table"></param>
+    /// <param name="key"></param>
+    /// <returns></returns>
     public string GetLocalizedString(string table, string key)
     {
         // Try to find it in this cache then return
