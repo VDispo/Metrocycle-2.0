800c45e1a71add97fcfffb893731616dd791717f
shankencedric <legara.cedric@gmail.com>
2025-06-07 00:09:58 +0800

    style: delete a few commented lines

--- Assets/Scripts/SpecialConditions/RainConditionHandler.cs
+++ Assets/Scripts/SpecialConditions/RainConditionHandler.cs
@@ -24,12 +24,7 @@ public class RainConditionHandler : ConditionHandler
         startOffsetZ = rainParticlesTransform.localPosition.z;
         StartCoroutine(nameof(UpdateParticlePosition));
     }
-
-    private void Update()
-    {
-        Skid();
-    }
-
+    
     private IEnumerator UpdateParticlePosition()
     {
         rainParticlesTransform.localPosition = new Vector3(
@@ -38,30 +33,6 @@ public class RainConditionHandler : ConditionHandler
             activeVehicle.localPosition.z + startOffsetZ);
         yield return new WaitForSecondsRealtime(particlesPositionUpdateInterval);
         StartCoroutine(nameof(UpdateParticlePosition));
-    }
-
-    // this might need to be implemented inside ArcadeBikeController.cs and BicycleVehicle.cs
-    private void Skid()
-    {
-        // not sure abt this part
-        // is it just: theres a % chance that when u break in one go, [what is da % chance]
-        // that you wouldnt stop cuz u'd be skidding,
-        // then play skidding sound,
-        // then if too much, show skidding prompt and restart to last checkpoint
-
-        // so like:
-        // if speed > x (unknown speed x for now)
-        // AND brake was held for n milliseconds (unknown tolerance n for now)
-        // AND rolled for the % skid chance (unknown % for now),
-        // THEN % of original speed is kept (unknown % for now) as skidding speed
-        // AND play skidding sound
-        // AND cant (?) use any controls other than brakes (?)
-        // AND programmatically remember this was a skid (so next prompts could reference "u bumped to someone cuz u skid'd"
-        // AND if bumped, do popup the modified (skid) prompt
-        // AND restart to last checkpoint
-
-        // idea ni balong:
-        // less turn angle the faster it is
     }
 
     private void OnDestroy()
